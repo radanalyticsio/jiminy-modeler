e10569fce57bc2bf48132265dd2123141ac0feae
sophwats <sowatson@redhat.com>
2017-11-09 14:04:16 +0000

    added pyspark modules

--- postgresql_progress.py
+++ postgresql_progress.py
@@ -1,6 +1,7 @@
 import psycopg2
 import pyspark
-
+from pyspark.sql import SparkSession, SQLContext
+from pyspark.sql.functions import mean, desc
 ####
 try:
     con = psycopg2.connect(dbname='movielens', user='postgres', host='localhost', password='password')
