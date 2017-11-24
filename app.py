84cf838f6fb8020bab83b5281d971a76b0ae2b7d
Rui  Vieira <ruidevieira@googlemail.com>
2017-11-24 10:25:58 +0000

    Added logging

--- app.py
+++ app.py
@@ -1,66 +1,78 @@
 #!/usr/bin/env python
 """main app file for modeler"""
 
-
 import psycopg2
-from pyspark.sql import SQLContext
 from pyspark import SparkContext, SparkConf
 import modeller
 import storage
 import sys
+import logging
+
 
 def main():
-    # just leaving these here for future reference (elmiko)
-    # from pyspark import sql as pysql
-    # spark = pysql.SparkSession.builder.appName("JiminyMod").getOrCreate()
-    # sc = spark.sparkContext
-    ### set up the spark context.
+
+    logger = logging.getLogger("jiminy-modeler")
+    ch = logging.StreamHandler()
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    # set up the spark context.
+
+    logger.debug("Connecting to Spark")
+
     conf = SparkConf().setAppName("recommender")
     conf = (conf.setMaster('local[*]')
             .set('spark.executor.memory', '4G')
             .set('spark.driver.memory', '45G')
             .set('spark.driver.maxResultSize', '10G'))
     sc = SparkContext(conf=conf)
-    sqlContext=SQLContext(sc)
-    #Set up SQL Context
+
+    # Set up SQL Context
+
     try:
         con = psycopg2.connect(dbname='movielens', user='postgres', host='localhost', port='5432', password='password')
-    #    print "Connected to database"
     except:
+        logger.error("Could not connect to data store")
         sys.exit(1)
-     # fetch the data from the db
-    cursor=con.cursor()
+
+    # fetch the data from the db
+    cursor = con.cursor()
     cursor.execute("SELECT * FROM ratingsdata")
 
     ratings = cursor.fetchall()
-    #creates the RDD:
+
+    # creates the RDD:
     ratingsRDD = sc.parallelize(ratings)
-    #currently removing the final column, which contains the time stamps.
+
+    # currently removing the final column, which contains the time stamps.
     ratingsRDD = ratingsRDD.map(lambda x: (x[0], x[1], x[2]))
 
     estimator = modeller.Estimator(ratingsRDD)
-    ### basic parameter selection ###
+
+    # basic parameter selection
     parameters = estimator.run(ranks=[2, 4, 6, 8],
-                               lambdas=[0.01,0.05, 0.09, 0.13],
+                               lambdas=[0.01, 0.05, 0.09, 0.13],
                                iterations=[2])
-    ### trains the model
+    # trains the model
     model = modeller.Trainer(data=ratingsRDD,
                              rank=parameters['rank'],
                              iterations=parameters['iteration'],
-                             lambda_ = parameters['lambda'],
+                             lambda_=parameters['lambda'],
                              seed=42).train()
-    ### writes the model
+    # writes the model
     writer = storage.MongoDBModelWriter(host='localhost', port=27017)
     writer.write(model=model, version=1)
 
-    #while True:
-        # this loop should be at the heart of this application, it will
-        # continually loop until killed by the orchestration engine.
-        # in this loop it should generally do the following:
-        # 1. check to see if it should create a new model
-        # 2. if yes, create a new model. if no, continue looping
-        #    (perhaps with a delay)
-        # 3. store new model
+    # while True:
+    # this loop should be at the heart of this application, it will
+    # continually loop until killed by the orchestration engine.
+    # in this loop it should generally do the following:
+    # 1. check to see if it should create a new model
+    # 2. if yes, create a new model. if no, continue looping
+    #    (perhaps with a delay)
+    # 3. store new model
+
 
 if __name__ == '__main__':
     main()
