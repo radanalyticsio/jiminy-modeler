ec587d6b61c2839683398b0217cb3ec0f26110e9
sophwats <sowatson@redhat.com>
2017-11-29 20:36:55 +0000

    Model Writer requires a URI and spark context

--- storage.py
+++ storage.py
@@ -10,8 +10,14 @@ class ModelWriter:
     """
     __metaclass__ = ABCMeta
 
-    def __init__(self):
-        pass
+    def __init__(self, sc, uri):
+        """
+
+        :param sc: A Spark context
+        :param uri: The connection URI
+        """
+        self._sc = sc
+        self._url = uri
 
     @abstractmethod
     def write(self, model, version):
@@ -29,10 +35,10 @@ class MongoDBModelWriter(ModelWriter):
     Model store writer to a MongoDB backend
     """
 
-    def __init__(self, host='localhost', port=27017):
-        super(MongoDBModelWriter, self).__init__()
-        self._client = MongoClient(host=host, port=port)
-        self._db = self._client.models
+    def __init__(self, sc, uri):
+        super(MongoDBModelWriter, self).__init__(sc=sc, uri=uri)
+        client = MongoClient(self._url)
+        self._db = client.models
 
     def write(self, model, version):
 
