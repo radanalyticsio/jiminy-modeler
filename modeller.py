604b9c96541650ac878784d793c7cf034a7b3755
sophwats <sowatson@redhat.com>
2018-02-12 16:48:25 -0500

    Added docstrings

--- modeller.py
+++ modeller.py
@@ -11,36 +11,41 @@ import numpy as np
 
 
 class Estimator:
+    """Estimator class for Project Jiminy.
+    Used to determine Model parameters.
+    """
     def __init__(self, data):
         self._data = data
         #std bootstrap proportions for the training, validation and testing
         self._sets = self._split([0.6, 0.2, 0.2])
 
-
     def _split(self, proportions):
+        """Split data into three random chunks."""
         split = self._data.randomSplit(proportions)
         return {'training': split[0], 'validation': split[1], 'test': split[2]}
 
     def rmse(self, model):
+        """Compute root mean squared error for the validation set."""
         predictions = model.predictAll(self._sets['validation'].map(lambda x: (x[0], x[1])))
         predictions_rating = predictions.map(Estimator.group_ratings)
         validation_rating = self._sets['validation'].map(Estimator.group_ratings)
         joined = validation_rating.join(predictions_rating)
         return math.sqrt(joined.map(lambda x: (x[1][0] - x[1][1]) ** 2).mean())
 
-    # return ((userId, movieId), rating)
     @staticmethod
     def group_ratings(x):
+        """Return ((userId, movieId), rating)."""
         return ((int(x[0]), int(x[1])), float(x[2]))
 
     def _train(self, rank, iterations, lambda_, seed):
+        """Train a model, using the given parameters."""
         return pyspark.mllib.recommendation.ALS.train(ratings=self._sets['training'],
                          rank=rank, seed=seed,
                          lambda_=lambda_,
                          iterations=iterations)
 
     def run(self, ranks, lambdas, iterations):
-        # create combinations of parameters to test
+        """Return optimal parameters from given input sets."""
         rmses = []
         combos=[]
         sizings = [len(ranks), len(lambdas), len(iterations)]
@@ -75,6 +80,7 @@ class Estimator:
 
 
 class Trainer:
+    """Train the ALS model."""
     def __init__(self, data, rank, iterations, lambda_, seed):
         self._data = data
         self.rank = rank
