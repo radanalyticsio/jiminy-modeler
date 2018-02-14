7f7f712aa4d131b26b949e3560996479eef11300
sophwats <sowatson@redhat.com>
2018-02-14 10:32:36 -0500

    Logger.py conforms with pep8

--- logger.py
+++ logger.py
@@ -5,8 +5,9 @@ import logging
 def get_logger():
     logger = logging.getLogger("jiminy-modeller")
     if not logger.handlers:
-        ch=logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s-%(message)s')
+        ch = logging.StreamHandler()
+        formatter = logging.Formatter(
+                    '%(asctime)s - %(name)s - %(levelname)s-%(message)s')
         ch.setFormatter(formatter)
         logger.addHandler(ch)
         logger.setLevel(logging.DEBUG)
